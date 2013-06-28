018174eb9a791b6c84f09117f53deffff5c43d59
TJ Holowaychuk <tj@vision-media.ca>
2013-06-28 14:40:37 -0700

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,54 @@
+
+# xhr-image
+
+  XHR-driven `Image` for progress events.
+
+## Installation
+
+  Install with [component(1)](http://component.io):
+
+    $ component install component/xhr-image
+
+## Example
+
+``js
+var XHRImage = require('xhr-image');
+
+var img = new XHRImage('http://i.cloudup.com/0chKjveyrS.png');
+
+img.on('progress', function(e){
+  progress.style.width = (e.percent | 0) + '%';
+});
+
+img.on('load', function(){
+  var el = new Image;
+  el.src = URL.createObjectURL(img.xhr.response);
+  document.body.appendChild(el);
+});
+```
+
+## Events
+
+ - `open`
+ - `sent`
+ - `receiving`
+ - `load`
+ - `progress`
+
+## API
+
+### Image(src)
+
+  Request the given img `src`.
+
+### .xhr
+
+  The XHR request.
+
+### .abort()
+
+  Abort the request.
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,77 @@
+
+/**
+ * Module dependencies.
+ */
+
+var Emitter = require('emitter');
+
+/**
+ * Expose `Image`.
+ */
+
+module.exports = Image;
+
+/**
+ * Initialize an `Image` with `src`.
+ *
+ * @param {String} src
+ * @api public
+ */
+
+function Image(src) {
+  if (!src) throw new TypeError('src required');
+  this.xhr = new XMLHttpRequest;
+  this.xhr.open('GET', src, true);
+  this.xhr.responseType = 'blob';
+  this.xhr.onprogress = this.onprogress.bind(this);
+  this.xhr.onreadystatechange = this.onchange.bind(this);
+  this.xhr.send();
+}
+
+/**
+ * Mixin emitter.
+ */
+
+Emitter(Image.prototype);
+
+/**
+ * Abort the request.
+ *
+ * @api public
+ */
+
+Image.prototype.abort = function(){
+  this.xhr.abort();
+};
+
+/**
+ * Handle progress.
+ */
+
+Image.prototype.onprogress = function(e){
+  e.percent = e.loaded / e.total * 100;
+  this.emit('progress', e);
+};
+
+/**
+ * Handle state changes.
+ */
+
+Image.prototype.onchange = function(){
+  var state = this.xhr.readyState;
+
+  switch (state) {
+    case 1:
+      this.emit('open');
+      break;
+    case 2:
+      this.emit('sent');
+      break;
+    case 3:
+      this.emit('receiving');
+      break;
+    case 4:
+      this.emit('load');
+      break;
+  }
+};
